9a45dd7c3900fbcf2fdd9ec6c0d7b4a4f335d6f0
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-12-20 10:42:43 -0500

    build: water to the bottom of the screen, waving surface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html lang="en">
+      <head>
+          <title>Making Waves</title>
+      </head>
+      <body>
+        <div className="wavebox">
+          <svg className="water">
+            <path
+              fill="#454599"
+              d="
+                M 0,100
+                C 273,183
+                  822,-40
+                  1920,106
+
+                V 800
+                H 0
+                V 67
+                Z">
+              <animate
+                repeatCount="indefinite"
+                fill="#454599"
+                attributeName="d"
+                dur="6s"
+                values="
+                  M 0 67
+                  C 473,283
+                    822,-40
+                    1920,116
+
+                  V 800
+                  H 0
+                  V 67
+                  Z;
+
+                  M 0 57
+                  C 473,-40
+                    1222,283
+                    1920,136
+
+                  V 800
+                  H 0
+                  V 67
+                  Z;
+
+                  M 0 77
+                  C 973,260
+                    1722,-53
+                    1920,120
+
+                  V 800
+                  H 0
+                  V 67
+                  Z;
+
+                  M 0 67
+                  C 473,283
+                    822,-40
+                    1920,116
+
+                  V 800
+                  H 0
+                  V 67
+                  Z;
+                "
+              ></animate>
+            </path>
+          </svg>
+        </div>
+      </body>
+    </html>
   );
 }
 
